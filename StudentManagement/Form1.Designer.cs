a5e3bae3195fd437a1891a3d61ed1bb59320a023
Huỳnh Gia Phú <90060539+PhuGiaHuynh@users.noreply.github.com>
2022-10-19 13:33:03 +0700

    commit

---
+++ StudentManagement/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace StudentManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mssv = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hoten = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.malop = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.diemtb = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveData = new System.Windows.Forms.Button();
+            this.studentLv = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.deleteRow = new System.Windows.Forms.Button();
+            this.updateData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mssv
+            // 
+            this.mssv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mssv.Location = new System.Drawing.Point(195, 95);
+            this.mssv.Name = "mssv";
+            this.mssv.Size = new System.Drawing.Size(200, 28);
+            this.mssv.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quản lý sinh viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã số sinh viên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(86, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Họ và tên";
+            // 
+            // hoten
+            // 
+            this.hoten.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoten.Location = new System.Drawing.Point(195, 158);
+            this.hoten.Name = "hoten";
+            this.hoten.Size = new System.Drawing.Size(200, 28);
+            this.hoten.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Mã lớp";
+            // 
+            // malop
+            // 
+            this.malop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.malop.Location = new System.Drawing.Point(195, 223);
+            this.malop.Name = "malop";
+            this.malop.Size = new System.Drawing.Size(200, 28);
+            this.malop.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(32, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Điểm trung bình";
+            // 
+            // diemtb
+            // 
+            this.diemtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diemtb.Location = new System.Drawing.Point(195, 287);
+            this.diemtb.Name = "diemtb";
+            this.diemtb.Size = new System.Drawing.Size(200, 28);
+            this.diemtb.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(454, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(273, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // saveData
+            // 
+            this.saveData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveData.Location = new System.Drawing.Point(90, 367);
+            this.saveData.Name = "saveData";
+            this.saveData.Size = new System.Drawing.Size(113, 38);
+            this.saveData.TabIndex = 10;
+            this.saveData.Text = "Thêm";
+            this.saveData.UseVisualStyleBackColor = true;
+            this.saveData.Click += new System.EventHandler(this.saveData_Click);
+            // 
+            // studentLv
+            // 
+            this.studentLv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.studentLv.FullRowSelect = true;
+            this.studentLv.HideSelection = false;
+            this.studentLv.Location = new System.Drawing.Point(770, 40);
+            this.studentLv.MultiSelect = false;
+            this.studentLv.Name = "studentLv";
+            this.studentLv.Size = new System.Drawing.Size(607, 365);
+            this.studentLv.TabIndex = 13;
+            this.studentLv.UseCompatibleStateImageBehavior = false;
+            this.studentLv.View = System.Windows.Forms.View.Details;
+            this.studentLv.SelectedIndexChanged += new System.EventHandler(this.studentLv_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã số sinh viên";
+            this.columnHeader1.Width = 108;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Họ và tên";
+            this.columnHeader2.Width = 130;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Lớp";
+            this.columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Điểm trung bình";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 131;
+            // 
+            // deleteRow
+            // 
+            this.deleteRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteRow.Location = new System.Drawing.Point(407, 367);
+            this.deleteRow.Name = "deleteRow";
+            this.deleteRow.Size = new System.Drawing.Size(107, 38);
+            this.deleteRow.TabIndex = 14;
+            this.deleteRow.Text = "Xóa";
+            this.deleteRow.UseVisualStyleBackColor = true;
+            this.deleteRow.Click += new System.EventHandler(this.deleteRow_Click);
+            // 
+            // updateData
+            // 
+            this.updateData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateData.Location = new System.Drawing.Point(234, 367);
+            this.updateData.Name = "updateData";
+            this.updateData.Size = new System.Drawing.Size(142, 38);
+            this.updateData.TabIndex = 15;
+            this.updateData.Text = "Cập nhật";
+            this.updateData.UseVisualStyleBackColor = true;
+            this.updateData.Click += new System.EventHandler(this.updateData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1425, 463);
+            this.Controls.Add(this.updateData);
+            this.Controls.Add(this.deleteRow);
+            this.Controls.Add(this.studentLv);
+            this.Controls.Add(this.saveData);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.diemtb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.malop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.hoten);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mssv);
+            this.Name = "Form1";
+            this.Text = "Quản lý sinh viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mssv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox hoten;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox malop;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox diemtb;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button saveData;
+        private System.Windows.Forms.ListView studentLv;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button deleteRow;
+        private System.Windows.Forms.Button updateData;
+    }
+}
+
---
+++ StudentManagement/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace StudentManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string file_name = "d:\\dssv.txt";
+        public void LoadData()
+        {
+            if (File.Exists(file_name))
+            {
+                string[] data = File.ReadAllLines(file_name);
+                foreach (string line in data)
+                {
+                    string[] col = line.Split('-');
+                    ListViewItem item = new ListViewItem();
+                    item.Text = col[0];
+                    studentLv.Items.Add(item);
+
+                    ListViewItem.ListViewSubItem subitem1 = new ListViewItem.ListViewSubItem(item, col[1]);
+                    item.SubItems.Add(subitem1);
+
+                    ListViewItem.ListViewSubItem subitem2 = new ListViewItem.ListViewSubItem(item, col[2]);
+                    item.SubItems.Add(subitem2);
+
+                    ListViewItem.ListViewSubItem subitem3 = new ListViewItem.ListViewSubItem(item, col[3]);
+                    item.SubItems.Add(subitem3);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void saveData_Click(object sender, EventArgs e)
+        {
+            if ((string.IsNullOrEmpty(mssv.Text)) || (string.IsNullOrEmpty(hoten.Text)) || (string.IsNullOrEmpty(malop.Text)) || (string.IsNullOrEmpty(diemtb.Text)))
+            {
+                MessageBox.Show("Vui lòng điền đủ thông tin");
+            }
+            else
+            {
+                string[] data = File.ReadAllLines(file_name);
+                foreach (string line in data)
+                {
+                    string[] col = line.Split('-');
+                    if (col[0] == mssv.Text)
+                    {
+                        MessageBox.Show("Mã số sinh viên đã tồn tại !");
+                        return;
+                    }
+                }
+
+                // Ghi dữ liệu vào file txt
+                File.AppendAllText(file_name, mssv.Text + "-");
+                File.AppendAllText(file_name, hoten.Text + "-");
+                File.AppendAllText(file_name, malop.Text + "-");
+                File.AppendAllText(file_name, diemtb.Text + "\n");
+
+                MessageBox.Show("Thông tin sinh viên đã được lưu !!");
+                mssv.Clear();
+                hoten.Clear();
+                malop.Clear();
+                diemtb.Clear();
+                studentLv.Items.Clear();
+                LoadData();
+            }
+        }
+
+        private void deleteRow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Đã xóa thông tin sinh viên!");
+            studentLv.Items.Remove(studentLv.SelectedItems[0]);
+            List<string> data = new List<string>();
+            File.WriteAllText(file_name, "");
+            foreach (ListViewItem item in studentLv.Items)
+            {
+                data.Add(item.Text + "-" + item.SubItems[1].Text + "-" + item.SubItems[2].Text + "-" + item.SubItems[3].Text);
+            }
+            data.ToArray();
+            File.WriteAllLines(file_name, data);
+            mssv.Clear();
+            hoten.Clear();
+            malop.Clear();
+            diemtb.Clear();
+        }
+        private void studentLv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (studentLv.SelectedItems.Count == 0)
+            {
+                mssv.Text = "";
+                hoten.Text = "";
+                malop.Text = "";
+                diemtb.Text = "";
+            }
+            else
+            {
+                ListViewItem item = studentLv.SelectedItems[0];
+                mssv.Text = item.Text;
+                hoten.Text = item.SubItems[1].Text;
+                malop.Text = item.SubItems[2].Text;
+                diemtb.Text = item.SubItems[3].Text;
+            }
+        }
+
+        private void updateData_Click(object sender, EventArgs e)
+        {
+            if (studentLv.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Chưa chọn sinh viên để cập nhật !");
+                return;
+            }
+            MessageBox.Show("Đã cập nhật thông tin sinh viên!");
+            studentLv.SelectedItems[0].Text = mssv.Text;
+            studentLv.SelectedItems[0].SubItems[1].Text = hoten.Text;
+            studentLv.SelectedItems[0].SubItems[2].Text = malop.Text;
+            studentLv.SelectedItems[0].SubItems[3].Text = diemtb.Text;
+            List<string> data = new List<string>();
+            File.WriteAllText(file_name, "");
+            foreach (ListViewItem item in studentLv.Items)
+            {
+                data.Add(item.Text + "-" + item.SubItems[1].Text + "-" + item.SubItems[2].Text + "-" + item.SubItems[3].Text);
+            }
+            data.ToArray();
+            File.WriteAllLines(file_name, data);
+            mssv.Clear();
+            hoten.Clear();
+            malop.Clear();
+            diemtb.Clear();
+        }
+    }
+}
